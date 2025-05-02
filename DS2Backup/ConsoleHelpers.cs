955c8afbe7c5b9c63945ed0adfd5ffbd7c7b57d5
Feor <5939320+FeorPrime@users.noreply.github.com>
2025-05-02 17:29:21 +0300

    initials

---
+++ DS2Backup/ConsoleHelpers.cs
@@ -0,0 +1,32 @@
+﻿namespace DS2Backup;
+
+public static class ConsoleHelpers
+{
+    public static void PrintSuccess(this string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+
+    public static void PrintError(this string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+
+    public static void PrintWarning(this string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+
+    public static void PrintInfo(this string message)
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+}
---
+++ readme.md
@@ -0,0 +1,14 @@
+# DS save backup manager (another one)
+
+1. Create a backup folder with name corresponding to hex steam id
+2. zip all files from ds save directory to the backup folder with specified name \ tag
+3. restore from backup folder specified checkpoint
+4. some ui
+5. watch mode with auto backups at specified time periods
+
+## Additions:
+
+1. checkup player's name (and may be show it)
+2. show player's avatar
+3. show game preview xD (or idk, character preview like in game) and some saved game stats that can be retrieved from save file
+4. add some options of cloud saving
